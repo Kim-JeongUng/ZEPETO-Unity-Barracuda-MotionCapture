134023750e64f0849e5654ec5ca54549ab9d0127
K-JeongUng <82865325+Kim-JeongUng@users.noreply.github.com>
2022-05-13 21:23:05 +0900

    Zepeto character call Complete

--- Assets/CharacterController.ts
+++ Assets/CharacterController.ts
@@ -6,7 +6,7 @@ export default class CharacterController extends ZepetoScriptBehaviour {
 
     Start() {
         console.log("Hell");
-        ZepetoPlayers.instance.CreatePlayerWithZepetoId("ajh322", "ajh322", new SpawnInfo(), true);
+        ZepetoPlayers.instance.CreatePlayerWithZepetoId("develop_ung", "develop_ung", new SpawnInfo(), true);
         ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener(() => {
             let _player = ZepetoPlayers.instance.LocalPlayer;
         })
